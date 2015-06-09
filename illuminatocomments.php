4babc28b8348b2c9598bae6fd80d1eafdd9e4d56
Andréas Livet <andreas.livet@gmail.com>
2015-06-09 15:02:53 +0200

    Module configuration is now loaded automatically in the parent class

--- illuminatocomments.php
+++ illuminatocomments.php
@@ -2,18 +2,9 @@
 if (!defined('_PS_VERSION_'))
 	exit;
 
-class IlluminatoComments extends Illuminato\Module {
+class IlluminatoComments extends Illuminato\IllModule {
 	public function __construct() {
-		//Instead of giving the name, just give the _FILE_ so we can automatically check if the filename,
-		//the directory and the name are the same
-		$this->file = __FILE__;
-		$this->tab = 'front_office_features';
-		$this->version = '0.1';
-		$this->author = 'Fabien Serny';
-		//bootstrap option is already set to true
-		//and prestashop version requirement is set to 1.6 min
+		//Needed cause parent class retrieve filename with debug_backtrace
 		parent::__construct();
-		$this->displayName = $this->l('My Module of product comments');
-		$this->description = $this->l('With this module, your customers will be able to grade and comments your products.');
 	}
 }
