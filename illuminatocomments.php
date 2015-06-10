3ba4b06e3147da3c094f3f6093e90527fc7246b5
Andréas Livet <andreas.livet@gmail.com>
2015-06-10 17:01:55 +0200

    constructor is no more needed

--- illuminatocomments.php
+++ illuminatocomments.php
@@ -3,10 +3,6 @@ if (!defined('_PS_VERSION_'))
 	exit;
 
 class IlluminatoComments extends Illuminato\Module {
-	public function __construct() {
-		//Needed cause parent class retrieve filename with debug_backtrace
-		parent::__construct();
-	}
 }
 
 //Artisan::call('migrate');//->run('path/to/migrations');
