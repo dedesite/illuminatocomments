318d31173394832408670f8799471af9f82f605f
Andréas Livet <andreas.livet@gmail.com>
2015-12-01 08:55:41 +0100

    Start using the Prestashop conf utility

--- illuminatocomments.php
+++ illuminatocomments.php
@@ -5,6 +5,18 @@ if (!defined('_PS_VERSION_'))
 	exit;
 
 class IlluminatoComments extends Illuminato\Module {
+
+	public function install()
+	{
+		// Call install parent method
+		if (!parent::install())
+			return false;
+		$this->conf->set(['GRADE' => '1', 'COMMENTS' => '1']);
+
+		return true;
+	}
+
+
 	public function hookDisplayProductTabContent($params)
 	{
 		$comment = new IlluminatoComments\Comments();
