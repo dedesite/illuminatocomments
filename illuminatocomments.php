b19489965874a2ef31bab00cd5fc659cd590e18e
Andréas Livet <andreas.livet@gmail.com>
2015-12-06 21:35:11 +0100

    Add some comments

--- illuminatocomments.php
+++ illuminatocomments.php
@@ -12,10 +12,11 @@ class IlluminatoComments extends Illuminato\Module {
 			'tab' => 'front_office_features',
 			'version' => '0.3',
 			'author' => 'Andréas Livet',
+			// Namespace use for localisation, config and views
+			'namespace' => 'comments',
 			// Note that we use Laravel's localization key system for localization
 			'displayName' => 'comments::module.name',
 			'description' => 'comments::module.description',
-			'namespace' => 'comments',
 		];
 	}
 
@@ -24,6 +25,7 @@ class IlluminatoComments extends Illuminato\Module {
 		// Call install parent method
 		if (!parent::install())
 			return false;
+		// This create prestashop config value with the ILLUMINATOCOMMENT_ prefix
 		$this->conf->set(['GRADE' => '1', 'COMMENTS' => '1']);
 
 		return true;
